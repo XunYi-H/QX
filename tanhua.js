e9d9178095b3603a9418d7304b81368a09a5c45f
XunYi-H <59787420+XunYi-H@users.noreply.github.com>
2021-01-26 14:39:19 +0800

    Create tanhua.js

---
+++ tanhua.js
@@ -0,0 +1,15 @@
+body = {
+  "status" : "200",
+  "msg" : "返回成功",
+  "data" : {
+    "mobile" : "18866889999",
+    "status" : 1,
+    "is_vip" : 1,
+    "member_name" : "TG@XunYi",
+    "head_pic" : "https://s3.ax1x.com/2021/01/26/sXoDTU.jpg",
+    "parent_id" : 22,
+    "user_viptime" : "2099-09-09"
+  }
+};
+
+$done(JSON.stringify(body));
