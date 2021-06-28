a7cbdc92c4e78b87ea6f798b640364a94aa83164
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-28 19:11:59 +0800

    Update qimaoreader.js

--- qimaoreader.js
+++ qimaoreader.js
@@ -11,5 +11,4 @@ hostname = xiaoshuo.wtzw.com,
 */
 let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
 obj.data.is_vip = "1";
-  };
 $done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
