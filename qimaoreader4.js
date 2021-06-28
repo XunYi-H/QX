987bf0a7b4e43cc715844f8b0bd64141e3a6be1f
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-28 21:55:27 +0800

    Update qimaoreader4.js

--- qimaoreader4.js
+++ qimaoreader4.js
@@ -6,13 +6,12 @@
 [rewrite_local]
 ^https:\/\/api-ks.wtzw.com\/api\/v2\/reader-adv\/index(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/qimaoreader4.js
 [mitm]
-hostname = xiaoshuo.wtzw.com,
+hostname = api-ks.wtzw.com,
 */
 //let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
-
 */
-var body = $response.body;
-body = body.replace('/.+/g','{"data": {"vip_list": ["点击屏幕中央可以呼出菜单"]}}');
+var body = $response.body;//声明一个body变量并以响应体赋值
+body = body.replace('/.+/g','000');
 //body = JSON.stringify(obj);
 $done(body); 
 //$done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
