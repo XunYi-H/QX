89929e99cdaa48163c4b0c421cfeb4bafb3c3b5e
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-28 20:56:16 +0800

    Update qimaoreader4.js

--- qimaoreader4.js
+++ qimaoreader4.js
@@ -8,8 +8,8 @@
 [mitm]
 hostname = xiaoshuo.wtzw.com,
 */
+//let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
 /*
-let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
 obj = {
   "data": {
     "vip_list": [
@@ -27,20 +27,6 @@ obj = {
   }
 };
 */
-body.replace(/.+,{
-  "data": {
-    "vip_list": [
-      "点击屏幕中央可以呼出菜单",
-      "提醒：要求事先打钱汇款的是诈骗",
-      "七猫看书永久免费",
-      "阅读设置里可以开启自动翻页",
-      "提醒：花呗套现刷额度的是诈骗",
-      "有问题可在我的-帮助与反馈里进行反馈",
-      "阅读设置里可以调整字体大小",
-      "七猫看书永久免费",
-      "提醒：网上汇款转账需警惕",
-      "更多阅读设置里可以隐藏右上角金币"
-    ]
-  }
-});
+var body = $response.body;
+body = body.replace(.+,{'"data": {"vip_list": ["点击屏幕中央可以呼出菜单"]}'});
 $done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
