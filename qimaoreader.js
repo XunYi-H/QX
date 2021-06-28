b8c7090f72ccbbc14487500ea4963420e89d3bdd
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-28 18:33:45 +0800

    Update qimaoreader.js

--- qimaoreader.js
+++ qimaoreader.js
@@ -4,7 +4,7 @@
 [rewrite_local]
 
 #七猫小说解锁终身会员+去广告
-^https:\/\/xiaoshuo.wtzw.com/api/v2/user/my-center(.+) url script-response-body //该地址
+^https:\/\/xiaoshuo.wtzw.com/api/v2/user/my-center(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/qimaoreader.js
 
 [mitm]
 hostname = xiaoshuo.wtzw.com,
@@ -12,8 +12,5 @@ hostname = xiaoshuo.wtzw.com,
 let obj = JSON.parse($response.body);
 obj = {
     data.is_vip: "1"
-  }
-}
-;
-
+  };
 $done({body: JSON.stringify(obj)});
