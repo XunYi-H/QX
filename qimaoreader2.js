7133838792801c66c4b77956317a077b9e30d803
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-28 19:54:23 +0800

    Create qimaoreader2.js

---
+++ qimaoreader2.js
@@ -0,0 +1,12 @@
+/*
+圈X:
+#七猫小说解锁终身会员+去广告
+#点击vip会员页显示终身vip
+[rewrite_local]
+^https:\/\/xiaoshuo.wtzw.com\/api\/v1\/user\/get(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/qimaoreader2.js
+[mitm]
+hostname = xiaoshuo.wtzw.com,
+*/
+let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
+obj.data.is_vip = "1";
+$done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
