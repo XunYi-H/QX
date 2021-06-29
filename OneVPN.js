a2f59275d0abe476a529206b3d9bcb8729f9c337
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-30 03:07:59 +0800

    Update OneVPN.js

--- OneVPN.js
+++ OneVPN.js
@@ -9,7 +9,7 @@
 hostname = one.sshub.top,
 */
 var body = $response.body;
-var str = JSON.stringify(body);
+var str = JSON.parse(body);
 var a = str.replace("\"payTime\":\{\"__type\":\"Date\",\"iso\":\"2021-06-30T07:39:36.993Z\"", "\"payTime\":\{\"__type\":\"Date\",\"iso\":\"2099-06-30T07:39:36.993Z\"");
 //var b = str.replace("true","true");
 $done({body: JSON.stringify(a)});//重新打包回json格式并结束修改
