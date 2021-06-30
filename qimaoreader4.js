e416b617ba81f593625d6ef67975fd27dc02c980
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-06-30 14:47:54 +0800

    Update qimaoreader4.js

--- qimaoreader4.js
+++ qimaoreader4.js
@@ -9,7 +9,8 @@
 hostname = api-ks.wtzw.com,
 */
 var body = $response.body;//获取响应消息体JSON
-var str = JSON.stringify(body);//将JSON转化成字符串
+//var str = JSON.stringify(body);//将JSON转化成字符串
+var str = JSON.parse(body);//将JSON转化成字符串
 var a = str.replace(/.+/g, "");//正则替换全部字符串为空
 var body = JSON.stringify(a);//重新打包回JSON字符串
 $done(body);//结束修改
