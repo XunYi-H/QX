ec4f494d72b0d17eb79df74444da3e29ab680886
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-07-01 20:26:28 +0800

    Update newyjl.js

--- newyjl.js
+++ newyjl.js
@@ -7,7 +7,7 @@
 
 [mitm]
 hostname = ac.centaur.cloud,
-
+*/
 let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
 obj.vip.expire = "2099-01-01T00:00:00.000000";
 $done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
