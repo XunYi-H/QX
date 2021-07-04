fa9ce62cb3574194d97ba699f4a8e0dd925e956f
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-07-05 05:20:55 +0800

    Update AniVPN.js

--- AniVPN.js
+++ AniVPN.js
@@ -3,7 +3,7 @@
 AniVPN unlock vip
 QX:
 [rewrite local]
-^https?:https:\/\/mamftei71za0evcs\.space\/api_account.php(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/AniVPN.js
+^https:https:\/\/mamftei71za0evcs\.space\/api_account\.php(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/AniVPN.js
 [mitm]
 hostname = mamftei71za0evcs.space
 */
@@ -11,6 +11,6 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj.message.end_time = 4070880000;
-
+obj.message.is_expired = 0;
 body = JSON.stringify(obj);
 $done({body});
