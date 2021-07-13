c5aa8a61accec8e2e518c231a05158e5c6bfeb61
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-07-13 16:35:53 +0800

    Update globalspeak2.js

--- globalspeak2.js
+++ globalspeak2.js
@@ -10,7 +10,7 @@ hostname = api.talkmate.com,
 let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
 obj.member_info.end_time = 4070880000;
 obj.member_info.member_type = 1;
-obj.member_info.money = 5000000;
+obj.member_info.money = 10000;
 obj.member_info.month_num = 12000;
-obj.member_info.start_time = 1609430400;
+obj.member_info.start_time = 999999999;
 $done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
