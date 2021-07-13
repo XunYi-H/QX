a322e90a84f39d671bfc9877182b290e46e8d28a
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-07-13 15:03:21 +0800

    Create globalspeak2.js

---
+++ globalspeak2.js
@@ -0,0 +1,16 @@
+  
+/*
+圈X:
+#全球说解锁vip
+[rewrite_local]
+^https:\/\/api.talkmate.com\/member\/check url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/globalspeak2.js
+[mitm]
+hostname = api.talkmate.com,
+*/
+let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
+obj.member_info.end_time = 4070880000;
+obj.member_info.member_type = 1;
+obj.member_info.money = 5000000;
+obj.member_info.month_num = 12000;
+obj.member_info.start_time = 1609430400;
+$done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
