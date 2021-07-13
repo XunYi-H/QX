899975baafb64c0cc52c512664afed8043a15f8d
TinyRainy0921 <85082345+TinyRainy0921@users.noreply.github.com>
2021-07-13 16:31:33 +0800

    Update globalspeak1.js

--- globalspeak1.js
+++ globalspeak1.js
@@ -3,11 +3,11 @@
 #全球说解锁vip
 
 [rewrite_local]
-^https:\/\/api.talkmate.com\/umvi\/user\/info(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/globalspeak1.js
+^https:\/\/api.talkmate.com\/umv1\/user\/info(.+) url script-response-body https://raw.githubusercontent.com/TinyRainy0921/ruler/main/globalspeak1.js
 [mitm]
 hostname = api.talkmate.com,
 */
-/*
+
 let obj = JSON.parse($response.body);//获取相应消息体（json格式）,并转换成对象处理
 obj.info.member_info.end_time = 4070880000;
 obj.info.member_info.member_type = 1;
@@ -15,6 +15,7 @@ obj.info.member_info.money = 0;
 obj.info.member_info.month_num = 999;
 obj.info.member_info.start_time = 999999999;
 $done({body: JSON.stringify(obj)});//重新打包回json格式并结束修改
-*/
+/*
 var body = $response.body.replace(/"end_time":\d+/g, '"end_time":1800000000').replace(/"member_type":\d/g, '"member_type":1').replace(/"money":\d+/g, '"money":999').replace(/"month_num":\d+/g, '"month_num":999').replace(/"start_time":\d+/g, '"start_time":1588174515').replace(/"money":\".*?"/g, '"money":0.00');
 $done({ body });
+*/
