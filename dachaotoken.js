b17bec8856ba1f2331ada5492ab7fea72f6c4a00
XunYi-H <59787420+XunYi-H@users.noreply.github.com>
2024-12-26 19:43:30 +0800

    Update dachaotoken.js

--- dachaotoken.js
+++ dachaotoken.js
@@ -1,3 +1,13 @@
+/*************************************
+
+[rewrite_local]
+https://m.aihoge.com/api/memberhy/h5/js/signature url script-request-header https://raw.githubusercontent.com/XunYi-H/QX/refs/heads/main/dachaotoken.js
+
+[mitm]
+hostname = m.aihoge.com
+
+*************************************/
+
 let headers = $request.headers;
 let member = headers['member'];
 
